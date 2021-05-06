e351b8be1f70bb712740217feaca7625b1c536db
Huang Jian-Min <yfr.huang@hotmail.com>
2021-05-06 17:02:01 +0800

    refactor

--- demo-spring-flux/src/main/kotlin/com/example/demo/spring/flux/service/TestService.kt
+++ demo-spring-flux/src/main/kotlin/com/example/demo/spring/flux/service/TestService.kt
@@ -1,10 +1,15 @@
 package com.example.demo.spring.flux.service
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
+import reactor.core.publisher.Mono
 
 @Service
 class TestService(
@@ -19,17 +24,15 @@ class TestService(
             val delay500req = getResponse(500).bodyToMono(String::class.java)
             val delay300req = getResponse(300).bodyToMono(String::class.java)
             val delay200req = getResponse(200).bodyToMono(String::class.java)
-            delay500req
-                .zipWith(delay300req) { a, b -> "$a / $b" }
-                .zipWith(delay200req) { a, b -> "$a / $b" }
+            Mono.zip(delay500req, delay300req, delay200req).map { v -> "${v.t1} / ${v.t2} / ${v.t3}" }
         }
 
     suspend fun coroutine() =
-        run {
-            val delay500req = getResponse(500).awaitBody<String>()
-            val delay300req = getResponse(300).awaitBody<String>()
-            val delay200req = getResponse(200).awaitBody<String>()
-            "$delay500req / $delay300req / $delay200req"
+        coroutineScope {
+            val delay500req = async(Dispatchers.IO) { getResponse(500).awaitBody<String>() }
+            val delay300req = async(Dispatchers.IO) { getResponse(300).awaitBody<String>() }
+            val delay200req = async(Dispatchers.IO) { getResponse(200).awaitBody<String>() }
+            "${delay500req.await()} / ${delay300req.await()} / ${delay200req.await()}"
         }
 
     private fun getResponse(ms: Long) = webClient.get()
