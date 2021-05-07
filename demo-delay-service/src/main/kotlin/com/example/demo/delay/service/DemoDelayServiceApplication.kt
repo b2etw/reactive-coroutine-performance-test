cd331b56c8ddf171088810781cfc04ba2ff79278
Huang Jian-Min <yfr.huang@hotmail.com>
2021-05-07 15:54:28 +0800

    add delay-service

---
+++ demo-delay-service/src/main/kotlin/com/example/demo/delay/service/DemoDelayServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo.delay.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoDelayServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoDelayServiceApplication>(*args)
+}
