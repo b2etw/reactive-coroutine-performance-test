13ead51fd2a9be3986f61fed35367b9d8cdbd9a2
Huang Jian-Min <yfr.huang@hotmail.com>
2021-05-07 15:18:29 +0800

    refactor

--- demo-vertx/src/main/kotlin/com/example/demo/vertx/TestVerticle.kt
+++ demo-vertx/src/main/kotlin/com/example/demo/vertx/TestVerticle.kt
@@ -1,9 +1,11 @@
 package com.example.demo.vertx
 
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.CompositeFuture
 import io.vertx.core.Promise
+import io.vertx.core.buffer.Buffer
+import io.vertx.ext.web.client.HttpResponse
 import io.vertx.ext.web.client.WebClient
-import io.vertx.ext.web.codec.BodyCodec
 import org.slf4j.LoggerFactory
 
 
@@ -13,17 +15,28 @@ class TestVerticle : AbstractVerticle() {
 
   override fun start(startPromise: Promise<Void>?) {
     vertx.eventBus().consumer<String>("test") {
-      WebClient.create(vertx)
-        .get(8888, "192.168.1.140", "/mock/delay/1")
+      val delay500req = WebClient.create(vertx)
+        .get(8888, "localhost", "/mock/delay/ms/500")
         .putHeader("Accept", "application/json")
         .send()
-        .onComplete { res ->
-          if (res.succeeded()) {
-            it.reply(res.result().bodyAsString())
-          } else {
-            log.error(res.cause().message, res.cause())
-          }
+      val delay300req = WebClient.create(vertx)
+        .get(8888, "localhost", "/mock/delay/ms/500")
+        .putHeader("Accept", "application/json")
+        .send()
+      val delay200req = WebClient.create(vertx)
+        .get(8888, "localhost", "/mock/delay/ms/500")
+        .putHeader("Accept", "application/json")
+        .send()
+      CompositeFuture.all(delay500req, delay300req, delay200req).onComplete { res ->
+        if (res.succeeded()) {
+          val resultAt0 = res.result().resultAt<HttpResponse<Buffer>>(0)
+          val resultAt1 = res.result().resultAt<HttpResponse<Buffer>>(1)
+          val resultAt2 = res.result().resultAt<HttpResponse<Buffer>>(2)
+          it.reply("${resultAt0.bodyAsString()} / ${resultAt1.bodyAsString()} / ${resultAt2.bodyAsString()}")
+        } else {
+          log.error(res.cause().message, res.cause())
         }
+      }
     }
   }
 }
