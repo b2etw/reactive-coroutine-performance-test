73dc810b1ef231587bbf9c95af2d05ee377c6417
Jian-Min Huang <vincent.huang@funpodium.net>
2021-07-06 19:59:11 +0800

    update

--- demo-spring-flux/src/main/kotlin/com/example/demo/spring/flux/service/disk/User.kt
+++ demo-spring-flux/src/main/kotlin/com/example/demo/spring/flux/service/disk/User.kt
@@ -1,6 +1,13 @@
 package com.example.demo.spring.flux.service.disk
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-data class User(var id: String?, var age: Int)
+data class User(
+
+    @Id
+    val id: String?,
+
+    val age: Int
+)
